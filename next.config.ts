b1bb6677226a35de0d3921c365db88f74ed4709c
robertbass <robertbass@hotmail.com>
2025-08-02 13:06:54 -0500

    PRODUCTS FROM DB AND LOADING EFFECT

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,16 @@ const nextConfig: NextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "src/sass")],
     prependData: `@import "main.sass"`
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.shopify.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   }
 };
 
--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,8 +1,44 @@
-export const MainProducts = () => {
+import Image from 'next/image'
+import styles from './MainProducts.module.sass'
+
+// Interfaces para tipar los productos de Shopify
+
+
+const getProducts = async () => {
+  try {
+    const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/products.json`, {
+      headers: new Headers({
+        'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || "",
+        //'Content-Type': 'application/json',
+      })
+    });
+    const { products } = await response.json();
+    return products;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+export const MainProducts = async () => {
+  const products = await getProducts();
+
   return (
     <section>
-      <h1>Main Products</h1>
-      {/* Add your main products content here */}
+      <h3>New products released!</h3>
+      <div className={styles.MainProducts__grid}>
+        {products?.map((product) => {
+          const imageSrc = product.images[0].src;
+          return (
+            <article key={product.id}>
+              <p>{product.title}</p>
+              <Image src={imageSrc} fill alt={product.title} loading='eager'/>
+
+            </article>
+          )
+        })}
+
+      </div>
     </section>
   );
 }
